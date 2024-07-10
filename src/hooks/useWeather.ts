d9729193e85425aa73d743fc3fa1410e8f8049e7
argi47 <argi47@msn.com>
2024-07-10 14:37:36 +0200

    añadidos zod y valibot para validar esquemas de respuesta api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,29 @@
 import axios from 'axios'
+import { z } from 'zod'
+// import { object, string, number, InferOutput, parse } from 'valibot'
 import { SearchType } from '../types'
+
+//  Zod
+const WeatherSchema = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  })
+})
+type Weather = z.infer<typeof WeatherSchema>
+
+//  Valibot
+// const WeatherSchema = object({
+//   name: string(),
+//   main: object({
+//     temp: number(),
+//     temp_max: number(),
+//     temp_min: number(),
+//   })
+// })
+// type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -19,7 +43,18 @@ export default function useWeather() {
 
       const { data: weatherData } = await axios.get(weatherUrl)
 
-      console.log(weatherData)
+      //  Zod
+      const result = WeatherSchema.safeParse(weatherData)
+      if (result.success) {
+        console.log(result.data.name)
+        console.log(result.data.main.temp)
+      }
+
+      //  Valibot
+      // const result = parse(WeatherSchema, weatherData)
+      // if (result) {
+      //   console.log(result)
+      // }
     }
     catch (error) {
       console.log(error)
