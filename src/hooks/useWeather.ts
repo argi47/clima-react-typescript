14bf8ea4bf34bebe92a68dfb0273bc5e831b622f
argi47 <argi47@msn.com>
2024-07-10 10:05:39 +0200

    llamada api para recuperar latitudes y longitudes

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,7 +1,20 @@
+import axios from 'axios'
+import { SearchType } from '../types'
+
 export default function useWeather() {
 
-  const fetchWeather = () => {
-    console.log('Consultando...')
+  const fetchWeather = async (search: SearchType) => {
+
+    const appId = '12f79a81494418b0ecb67baa30fb6996'
+
+    try {
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+      const { data } = await axios.get(url)
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
 
   return {
