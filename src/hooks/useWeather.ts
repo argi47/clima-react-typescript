567415237b222ccea7c4db6699f066b6edef4804
argi47 <argi47@msn.com>
2024-07-10 10:53:11 +0200

    añadida la segunda consulta contra la api

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -8,11 +8,18 @@ export default function useWeather() {
     const appId = import.meta.env.VITE_API_KEY
 
     try {
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
-      const { data } = await axios.get(url)
+      const { data } = await axios.get(geoUrl)
 
-      console.log(data)
+      const lat = data[0].lat
+      const lon = data[0].lon
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+      const { data: weatherData } = await axios.get(weatherUrl)
+
+      console.log(weatherData)
     }
     catch (error) {
       console.log(error)
